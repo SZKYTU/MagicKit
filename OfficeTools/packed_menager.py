efe7eaf9d8f017b6b36dfef80a2a68bc349b3974
Szkytu_Work <szkycode@gmail.com>
2023-09-04 09:30:17 +0200

    office module work!

--- OfficeTools/packed_menager.py
+++ OfficeTools/packed_menager.py
@@ -1,28 +1,55 @@
 import subprocess
 import questionary
+import os
+import win32api
+import win32con
 
 
 class OfficeTools:
     def run_office2013(self):
+        file_name = "2013.exe"
+        full_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), file_name)
+
         try:
-            subprocess.run(["OfficeTools\2013.exe"])
+            win32api.ShellExecute(
+                0, "open", full_file_path, None, None, win32con.SW_SHOWNORMAL)
         except FileNotFoundError:
-            print("Plik nie został znaleziony.")
+            print(
+                f"Plik '{full_file_path}' nie został znaleziony.")
 
     def run_office2016(self):
+        file_name = "2016.exe"
+        full_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), file_name)
+
         try:
-            subprocess.run(["2016.exe"])
+            win32api.ShellExecute(
+                0, "open", full_file_path, None, None, win32con.SW_SHOWNORMAL)
         except FileNotFoundError:
-            print("Plik nie został znaleziony.")
+            print(
+                f"Plik '{full_file_path}' nie został znaleziony.")
 
     def run_office2019(self):
+        file_name = "2019.exe"
+        full_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), file_name)
+
         try:
-            subprocess.run(["2019.exe"])
+            win32api.ShellExecute(
+                0, "open", full_file_path, None, None, win32con.SW_SHOWNORMAL)
         except FileNotFoundError:
-            print("Plik nie został znaleziony.")
+            print(
+                f"Plik '{full_file_path}' nie został znaleziony.")
 
     def run_office2021(self):
+        file_name = "2021.exe"
+        full_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), file_name)
+
         try:
-            subprocess.run(["2021.exe"])
+            win32api.ShellExecute(
+                0, "open", full_file_path, None, None, win32con.SW_SHOWNORMAL)
         except FileNotFoundError:
-            print("Plik nie został znaleziony.")
+            print(
+                f"Plik '{full_file_path}' nie został znaleziony.")
